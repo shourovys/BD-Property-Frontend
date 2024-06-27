8c8d7f5d191669ba73a985799fb338b91f9d7e55
Shourov Saha <shourovys@gmail.com>
2024-06-27 21:56:56 +0600

    feat: property list location info dynamic location

--- src/components/pages/propertyList/PropertyListLocationInfo.tsx
+++ src/components/pages/propertyList/PropertyListLocationInfo.tsx
@@ -1,15 +1,35 @@
-const PropertyListLocationInfo = () => {
+import { useAppSelector } from '@/hooks/reduxHooks'
+import Image from 'next/image'
+import React from 'react'
+
+const PropertyListLocationInfo: React.FC = () => {
+  const { selectedPropertyLocation } = useAppSelector(
+    (state) => state.propertySearch
+  )
+
+  const location = selectedPropertyLocation.length
+    ? {
+        label: selectedPropertyLocation
+          .map((location) => location.label)
+          .join(', '),
+      }
+    : { label: 'All Over Bangladesh' }
+
   return (
-    <div className='flex gap-2 rounded-6xs border border-lightgray-100 p-2'>
-      <img
-        className='h-[104px] w-[114px] rounded-6xs object-cover'
-        alt=''
-        src='/rectangle-27@2x.png'
+    <div className='flex gap-4 rounded-lg border border-lightgray-200 p-4 shadow-sm'>
+      <Image
+        className='rounded-lg object-cover'
+        alt={location.label}
+        src={'/rectangle-27@2x.png'}
+        width={114}
+        height={104}
+        placeholder='blur'
+        blurDataURL='/placeholder-image.png'
       />
-      <div className='p-1'>
-        <div className='font-medium'>Basundhara RA</div>
-        <div className='text-sm font-light leading-[25px] tracking-[0.03em]'>
-          Lorem ipsum dolor sit amet, consetetur{' '}
+      <div className='space-y-1'>
+        <div className='text-lg font-medium'>{location.label}</div>
+        <div className='text-sm font-light leading-relaxed text-gray-700'>
+          {`List of Properties of ${location.label}`}
         </div>
       </div>
     </div>
